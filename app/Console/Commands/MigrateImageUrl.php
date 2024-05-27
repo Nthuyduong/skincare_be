abd0d9111f0295477af8a2067b7f3a3b394a5aab
giangtx <36334907+giangtx@users.noreply.github.com>
2024-05-27 11:23:46 +0700

    add comment migrate img url

---
+++ app/Console/Commands/MigrateImageUrl.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MigrateImageUrl extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    // run command: php artisan migrate-blog-content-to-blog-details
+    protected $signature = 'migrate-blog-image-url';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $blogs = \App\Models\Blog::all();
+        foreach ($blogs as $blog) {
+            $blogDetail = new \App\Models\BlogDetail();
+            
+            $newContent = str_replace('https://app.radiance-aura.blog', 'https://api.radiance-aura.blog', $blogDetail->content);
+            $newContentDraft =  str_replace('https://app.radiance-aura.blog', 'https://api.radiance-aura.blog', $blogDetail->content_draft);
+
+            $blogDetail->content = $newContent;
+            $blogDetail->content_draft = $newContentDraft;
+
+            $blogDetail->save();
+        }
+    }
+}
