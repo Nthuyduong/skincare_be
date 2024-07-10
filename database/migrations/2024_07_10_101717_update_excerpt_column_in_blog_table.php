8887a0706691424bdd2bd4c6b03ee5add22cddbe
giangtx13 <giangtx13@gmail.com>
2024-07-10 17:23:11 +0700

    fix fix

--- database/migrations/2024_07_10_101717_update_excerpt_column_in_blog_table.php
+++ database/migrations/2024_07_10_101717_update_excerpt_column_in_blog_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('blogs', function (Blueprint $table) {
-            $table->string('excerpt', 500)->change();
+            $table->string('excerpt', 500)->nullable(true)->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('blogs', function (Blueprint $table) {
-            $table->string('excerpt')->change();
+            $table->string('excerpt')->nullable(true)->change();
         });
     }
 };
